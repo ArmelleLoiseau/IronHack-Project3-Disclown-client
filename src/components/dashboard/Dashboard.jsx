e2d85efd1f55e8a7be7a0ba07368f1b29f651fe0
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-02-05 19:59:42 +0100

    added chan and users context + redirection at sign-up

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,35 +1,41 @@
 import React, { useContext, useState } from "react";
 import socket from "../../socket";
+
+// Components
 import UsersList from "./UsersList";
 import Chat from "./Chat";
 import CreateChan from "./CreateChan";
 import ChanList from "./ChanList";
+
+// Contexts
+import { UserContext } from "../../context/user.context";
 // import { AuthContext } from "../../context/Auth.context";
 
 const Dashboard = () => {
   // const { isLoggedIn, user } = useContext(AuthContext);
-  const [users, setUsers] = useState([]);
-  const [chans, setChans] = useState([]);
+  // const [users, setUsers] = useState([]);
 
-  const addChan = (addedChan) => {
-setChans([...chans, addedChan])
-  };
   // const initReactiveProperties = (user) => {
   //   user.hasNewMessages = false;
   // };
 
+  // get all users from UserContext
+  const { users, setUsers } = useContext(UserContext);
+
   socket.on("users", (users) => {
+    // attach the "self" key to the current user
     users.forEach((user) => {
       user.self = user.userID === socket.id;
       // initReactiveProperties(user);
     });
+    // sort the array of users to that user.self is first
     users = users.sort((a, b) => {
       if (a.self) return -1;
       if (b.self) return 1;
       if (a.userEmail < b.userEmail) return -1;
       return a.userEmail > b.userEmail ? 1 : 0;
     });
-
+    // push new user in the array of user
     socket.on("user connected", (user) => {
       // initReactiveProperties(user);
       users.push(user);
@@ -44,9 +50,9 @@ setChans([...chans, addedChan])
   return (
     <div>
       <h1>Dashboard</h1>
-      <UsersList users={users} />
-      <CreateChan addChan={addChan}/>
-      <ChanList setChans={setChans} chans={chans} />
+      <UsersList />
+      <CreateChan />
+      <ChanList />
       <Chat />
       {/* {isLoggedIn && (
         <>
