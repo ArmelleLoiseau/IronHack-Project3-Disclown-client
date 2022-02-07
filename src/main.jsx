5ce7fef99917cc457038e2ff2506dfbf6cfa483c
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-02-07 20:52:09 +0100

    connection working again

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,18 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ChanContextWrapper } from "./context/chan.context";
 import { AuthProviderWrapper } from "./context/auth.context";
-
-
-import { AuthProviderWrapper } from "./context/auth.context";
 import { SocketProviderWrapper } from "./context/socket.context";
+import {UserContextWrapper} from "./context/user.context"
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProviderWrapper>
         <SocketProviderWrapper>
           <ChanContextWrapper>
+          <UserContextWrapper>
           <App />
+          </UserContextWrapper>
           </ChanContextWrapper>
         </SocketProviderWrapper>
       </AuthProviderWrapper>
